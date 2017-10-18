23383270b09d986598b914691a6c38227e1ea0c4
Liam McKersie <mckersiel@aldenv143.allegheny.edu>
2017-10-18 10:53:51 -0400

    Committing Lab Six

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,22 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
+
+/** This class has a method that manipulates a DNA string.
+  * The first line displays the name of the programmer and the date.
+  */
 public class ManipulateDna {
 
+  /** This program manioulates a DNA string. 
+    * Compile this program: javac ManipulateDna.java
+    * Run this program: java ManipulateDna
+    */
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Liam McKersie " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,17 +30,40 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dnaString = scanner.nextLine();
+    System.out.println("Okay, I am going to manipulate the DNA string \"" + dnaString + "\"");
+    System.out.println();
+    System.out.println("Enter a string only containing C, G, T and A: " + dnaString);
+    String complementDna = dnaString.toUpperCase();
     // Step Two: Compute the complement of the DNA String
-
+    complementDna = dnaString.replace('a', 'T');
+    complementDna = complementDna.replace('t', 'A');
+    complementDna = complementDna.replace('c', 'G');
+    complementDna = complementDna.replace('g', 'C');
+    System.out.println("Complement of " + dnaString + " is " + complementDna);
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    String dna = "actg";
+    Random random = new Random();
+    char letter = dna.charAt(random.nextInt(4));
+    int randomInsertionIndex = random.nextInt(dnaString.length());
+    String dnaMutationOne = dnaString.substring(0, randomInsertionIndex) 
+        + letter + dnaString.substring(randomInsertionIndex);
+    System.out.println("Inserting " + letter + " at position " + randomInsertionIndex 
+        + " gives " + dnaMutationOne);
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
+    String first = dnaString.substring(0, randomInsertionIndex);
+    String second = dnaString.substring(randomInsertionIndex + 1, dnaString.length());
+    System.out.println("Deleting from position " + randomInsertionIndex + " gives " + first 
+        + second);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    String three = dnaString.substring(0, randomInsertionIndex);
+    String four = dnaString.substring(randomInsertionIndex + 1, dnaString.length());
+    System.out.println("Changing position " + (randomInsertionIndex + 1) + " gives " + first
+        + letter + second);
     // Step Six: Display a final thankyou message
-
+    System.out.println();
+    System.out.println("Thank you for using the ManipulateDNA program.");
+    System.out.println("Have an awesome day.");
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,20 @@
-# Reflection by
+# Reflection by Liam McKersie
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to write a file called `ManipulateDNA`. Lab six was the
+most difficult lab thus far. The section that was most difficult to work
+through was step three. We were required to insert a randomly chosen DNA letter
+into the DNA String. Eventually, I used variables int and char to help proceed.
+I defined the int "randomInsertionIndex" as the variable that would represent
+where the randomly chosen DNA letter would fit into the string. Additionally I
+learned that it was necessary to use a "new Random" method to ensure that the
+DNA letter that is selected in each case is truly ranodm.
+
+Configuring a strategy to manipulate the DNA String was not simple. If I wrote
+commands to compute the complement of the DNA String, the DNA characters would
+swap twice and remain in their original places. I realized this was because I
+was replacing one upper case letter with another upper case letter. This forced
+me to command an upper case letter to swap with a lower case letter. With my
+new adjustment, the DNA letters only swapped once and remained in their
+randomized locations. Overall, lab six forced me to think on the fly and
+creatively adjust to errors. I am looking forward to applying my abilities to
+lab seven.
